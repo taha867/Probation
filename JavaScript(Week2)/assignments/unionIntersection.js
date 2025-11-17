0e877670ce853287a12f033f443e2411a8db4e1d
Muhammad Taha <muhammadtaha1780@gmail.com>
2025-11-17 18:28:42 +0500

    slight change in week 2 assignments folder

--- JavaScript(Week2)/assignments/unionIntersection.js
+++ JavaScript(Week2)/assignments/unionIntersection.js
@@ -1,14 +1,13 @@
 // Function to get Intersection and Union using Higher Order Functions
 function intersectionUnion(firstArr, secondArr) {
-  // Intersection 
+  // Intersection
   const intersection = firstArr
-    .filter(item => secondArr.includes(item))      // keep only common values
+    .filter((item) => secondArr.includes(item)) // keep only common values
     .filter((item, index, self) => self.indexOf(item) === index); // remove duplicates
 
-  // Union 
-  const union = [...firstArr, ...secondArr]    //spread operator        // merge both arrays
+  // Union
+  const union = [...firstArr, ...secondArr] //spread operator        // merge both arrays
     .filter((item, index, self) => self.indexOf(item) === index); // remove duplicates
-    
 
   return [intersection, union];
 }
@@ -16,30 +15,39 @@ function intersectionUnion(firstArr, secondArr) {
 function runTests() {
   const tests = [
     {
-      input: [[1, 2, 3, 4, 4], [4, 5, 9]],
-      expected: [[4], [1, 2, 3, 4, 5, 9]]
+      input: [
+        [1, 2, 3, 4, 4],
+        [4, 5, 9],
+      ],
+      expected: [[4], [1, 2, 3, 4, 5, 9]],
     },
     {
-      input: [[1, 2, 3], [4, 5, 6]],
-      expected: [[], [1, 2, 3, 4, 5, 6]]
+      input: [
+        [1, 2, 3],
+        [4, 5, 6],
+      ],
+      expected: [[], [1, 2, 3, 4, 5, 6]],
     },
     {
-      input: [[1, 1], [1, 1, 1, 1]],
-      expected: [[1], [1]]
+      input: [
+        [1, 1],
+        [1, 1, 1, 1],
+      ],
+      expected: [[1], [1]],
     },
     {
       input: [[], [1, 2]],
-      expected: [[], [1, 2]]
+      expected: [[], [1, 2]],
     },
     {
       input: [[7, 7, 7], []],
-      expected: [[], [7]]
-    }
+      expected: [[], [7]],
+    },
   ];
 
   tests.forEach((test, index) => {
     const result = intersectionUnion(test.input[0], test.input[1]);
-    console.log("UNION : ",result);
+    console.log("Intersection & UNION : ", result);
     const passed = JSON.stringify(result) === JSON.stringify(test.expected);
 
     console.log(`Test ${index + 1}: ${passed ? "PASSED ✅" : "FAILED ❌"}`);
